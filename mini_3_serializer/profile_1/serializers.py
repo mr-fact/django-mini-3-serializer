c927fc71296013832f6dd4bbc619dbbf6be94b1a
mr-fact <mr.fact.202020@gmail.com>
2023-09-16 12:48:08 +0330

    add simple serializers for profile, user and images

---
+++ mini_3_serializer/profile_1/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+
+from profile_1.models import MyUser, MyProfile, MyImage
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MyProfile
+        fields = [
+            'first_name',
+            'last_name',
+            'birthdate',
+        ]
+
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MyImage
+        fields = [
+            'image',
+        ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(required=False, many=True)
+    # profile = ProfileSerializer(required=False)
+
+    class Meta:
+        model = MyUser
+        fields = [
+            'username',
+            'password',
+            # 'images',
+            # 'profile',
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+    def save(self, **kwargs):
+        pass
