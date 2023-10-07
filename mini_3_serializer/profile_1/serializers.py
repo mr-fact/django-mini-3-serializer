5e1c6fd40a6bffcc63937d4cbdee0be480b4bc95
mr-fact <mr.fact.202020@gmail.com>
2023-10-08 00:37:11 +0330

    override validation functions

--- mini_3_serializer/profile_1/serializers.py
+++ mini_3_serializer/profile_1/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import empty
 
 from profile_1.models import MyUser, MyProfile, MyImage
 
@@ -39,11 +40,55 @@ class UserSerializer(serializers.ModelSerializer):
             'password': {'write_only': True, 'required': False}
         }
 
+    def is_valid(self, *, raise_exception=False):
+        print('\t+[deserializing->is_valid()]')
+        result = super().is_valid()
+        print('\t-[\t->is_valid()]')
+        return result
+
+    def run_validation(self, data=empty):
+        print('\t+[deserializing->run_validation()]')
+        result = super().run_validation(data)
+        print('\t-[\t->run_validation()]')
+        return result
+
+    def validate_empty_values(self, data):
+        print('\t+[deserializing->validate_empty_values()]')
+        result = super().validate_empty_values(data)
+        print('\t-[\t->validate_empty_values()]')
+        return result
+
+    def to_internal_value(self, data):
+        print('\t+[deserializing->to_internal_value()]')
+        result = super().to_internal_value(data)
+        print('\t-[\t->to_internal_value()]')
+        return result
+
+    def run_validators(self, value):
+        print('\t+[deserializing->run_validators()]')
+        result = super().run_validators(value)
+        print('\t-[\t->run_validators()]')
+        return result
+
+    def validate_username(self, data):
+        print('\t+[deserializing->field_validate()]')
+        result = data
+        print('\t-[\t->field_validate()]')
+        return result
+
+    def validate(self, attrs):
+        print('\t+[deserializing->validate()]')
+        result = attrs
+        print('\t-[\t->validate()]')
+        return result
+
     def to_representation(self, instance):
-        print('\t[serializing->to_representation()]')
+        print('\t+[serializing->to_representation()]')
         super_data = super().to_representation(instance)
         super_data['new_field'] = 'new_field_value'
-        return super_data
+        result = super_data
+        print('\t-[\t->to_representation()]')
+        return result
 
     def create(self, validated_data):
         instance = MyUser.objects.create_user(
